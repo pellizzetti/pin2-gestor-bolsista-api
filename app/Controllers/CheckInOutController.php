a0ad8b2627e6b0053b1af08458d26fa22bc5e227
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-16 18:35:20 -0200

    Cria método de check-in/out

--- app/Controllers/CheckInOutController.php
+++ app/Controllers/CheckInOutController.php
@@ -12,4 +12,34 @@ class CheckInOutController extends Controller
         parent::__construct($spot);
         $this->entity = 'App\Entities\CheckInOut';
     }
+
+    public function checkInOut()
+    {
+        $request = \Flight::request();
+        $data    = json_decode($request->getBody());
+
+        if (property_exists($data, 'inOut') && property_exists($data, 'userId')) {
+            $inOut  = $data->inOut;
+            $userId = $data->userId;
+
+            $mapper     = $this->spot->mapper($this->entity);
+            $checkInOut = $mapper->create([
+                'in_out'  => $inOut,
+                'user_id' => $userId
+            ]);
+                
+            \Flight::json(
+                array(
+                    'checked'    => true,
+                    'msg'        => "Check-{$inOut} realizado com sucesso",
+                    'checkInOut' => $checkInOut
+                )
+            );
+        }
+
+        \Flight::json(
+            'Servidor não pode entender a requisição por se tratar de uma sintaxe inválida para essa rota',
+            $code = 400
+        );
+    }
 }
