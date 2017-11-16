9363d5079199774346cd6bee549b6f58a2860950
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-16 18:35:29 -0200

    Adiciona rota de check-in/out

--- app/routes.php
+++ app/routes.php
@@ -30,3 +30,4 @@ Flight::route('/', function() {
 Flight::route('/user/@id:[0-9]{5}', array($UserController, 'getUserId'));
 Flight::route('GET /user/create/admin', array($UserController, 'createAdmin'));
 Flight::route('POST /login', array($UserController, 'loginByEmailAndPassword'));
+Flight::route('POST /checkinout', array($CheckInOutController, 'checkInOut'));
