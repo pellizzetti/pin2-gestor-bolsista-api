7851197bf51cc121e145517ddbe07a05ebb39453
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-25 14:45:07 -0200

    Seta header no Controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -16,6 +16,10 @@ abstract class Controller
      */
     public function __construct(Locator $spot)
     {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: PUT, GET, POST");
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
         $this->spot = $spot;
     }
 }
