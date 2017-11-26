ae22cd893dc348ea66e7132e0dcc8f135abec8ee
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-25 23:21:27 -0200

    Ajusta retorno jwt

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -62,7 +62,7 @@ class UserController extends Controller
                                 'userId'    => $user->id,
 				                'userName'  => $user->name,
                                 'userEmail' => $user->email,
-                                'userLevel' => 'admin'
+                                'userLevel' => $user->level
                             )
                         ),
                         'iss' => 'ceavi-udesc',
