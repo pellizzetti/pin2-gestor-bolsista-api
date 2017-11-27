478daeacfe815a3f8a0c0b2b51d5d49c56c66088
Guilherme Pellizzetti <guipellizzetti@gmail.com>
2017-11-27 14:28:50 -0200

    Adiciona controlador de atendimento, método de criação de atendimento e método de listagem de atendimentos

---
+++ app/Controllers/AttendanceController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Controllers;
+
+use Spot\Locator;
+use Firebase\JWT\JWT;
+
+class AttendanceController extends Controller
+{
+    public function __construct(Locator $spot)
+    {
+        parent::__construct($spot);
+        $this->entity = 'App\Entities\Attendance';
+    }
+
+    public function createAttendance()
+    {
+        $request = \Flight::request();
+        $data    = json_decode($request->getBody());
+
+        if (
+            property_exists($data, 'student') &&
+            property_exists($data, 'description') &&
+            property_exists($data, 'userId')
+        ) {
+            $attendanceStudent     = $data->student;
+            $attendanceDescription = $data->description;
+            $userId                = $data->userId;
+
+            try {
+                $mapper     = $this->spot->mapper($this->entity);
+                $attendance = $mapper->create([
+                    'student'     => $attendanceStudent,
+                    'description' => $attendanceDescription,
+                    'userId'      => $userId
+                ]);
+
+                return \Flight::json(
+                    array(
+                        'success'    => true,
+                        'message'    => 'Atendimento criado com sucesso',
+                        'attendance' => $attendance
+                    )
+                );
+            } catch (\Spot\Exception $e) {
+                return \Flight::json(
+                    array(
+                        'success' => false,
+                        'error'   => 'Erro ao criar atendimento',
+                        'message' => $e->getMessage()
+                    ),
+                    $code = 500
+                );
+            }
+        }
+
+        return \Flight::json(
+            array(
+                'success' => false,
+                'message' => 'Servidor não pode entender a requisição por se tratar de uma sintaxe inválida para essa rota'
+            ),
+            $code = 400
+        );
+    }
+
+    public function getListAttendance($userId)
+    {
+        if ($userId) {
+            $hoje           = new \DateTime('today');
+            $amanha         = new \DateTime('tomorrow');
+            $mapper         = $this->spot->mapper($this->entity);
+            $attendanceList = $mapper->where([
+                'user_id'       => $userId,
+                'created_at >=' => $hoje->format('Y-m-d H:i:s'),
+                'created_at <'  => $amanha->format('Y-m-d H:i:s'),
+            ])->order(['created_at' => 'asc']);
+                
+            if ($attendanceList) {
+                return \Flight::json(
+                    array(
+                        'success'        => true,
+                        'message'        => 'Lista de atendimentos retornada com sucesso',
+                        'attendanceList' => $attendanceList
+                    )
+                );
+            } 
+            return \Flight::json(
+                array(
+                    'success' => false,
+                    'message' => "Nenhum atendimento encontrado",
+                ),
+                $code = 401
+            );
+        }
+
+        return \Flight::json(
+            array(
+                'success' => false,
+                'message' => 'Servidor não pode entender a requisição por se tratar de uma sintaxe inválida para essa rota',
+            ),
+            $code = 400
+        );
+    }
+}
